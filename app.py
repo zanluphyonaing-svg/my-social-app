f06b432c082bf97673237cf27622f35325a4cdfd
zanluphyonaing-svg <zanluphyonaing@gmail.com>
2026-07-15 21:01:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///social_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///social_app_v2.db'app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Database Models
